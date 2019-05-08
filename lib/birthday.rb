1fe6f90524f355e2168e4950ea9103b569b90010
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-08 09:48:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,6 +10,14 @@ def happy_birthday(birthday_kids)
 end
 end 
 
+def age_appropriate_birthday 
+  if age>12 
+ 
+  birthday_kids.each do |kids_name,age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+end
+end 
+
 
 
 
